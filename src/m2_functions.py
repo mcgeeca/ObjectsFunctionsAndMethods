9342b5456bc85fc842c9d6b813e34be28c395b7d
mcgeeca <mcgeeca@pg.com>
2017-11-29 12:36:19 -0500

    Working on turtle4()

--- src/m2_functions.py
+++ src/m2_functions.py
@@ -3,12 +3,12 @@ Practice DEFINING and CALLING
      FUNCTIONS
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Craig McGee Jr.
 """
 
 ########################################################################
 #
-# TODO: 1. PUT YOUR NAME IN THE ABOVE LINE and...
+# DONE: 1. PUT YOUR NAME IN THE ABOVE LINE and...
 #
 #   Allow this file to use the rosegraphics.py file by marking the src
 #   directory as a "Sources Root".  Do that by right clicking on the src folder,
@@ -34,6 +34,7 @@ def main():
     window = rg.TurtleWindow()
 
     turtle1()
+    #turtle4()
     turtle3()
     turtle2()
     turtle2()
@@ -103,9 +104,16 @@ def turtle3():
     maja.end_fill()
 
 
+#def turtle4():
+    #cj = rg.SimpleTurtle()
+    #cj.pen = rg.Pen('green', 10)
+    #cj.forward(200)
+    #cj.right(90)
+    #cj.forward(100)
+
 ########################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   READ the code above.  Be sure you understand:
 #     -- How many functions are defined above?
 #           (Answer: 4)
@@ -137,7 +145,7 @@ def turtle3():
 
 ########################################################################
 #
-# TODO: 3.
+# DONE: 3.
 #   Define another function,
 #   immediately below the end of the definition of   turtle3   above.
 #   Name your new function   turtle4.
